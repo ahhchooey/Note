44b40fab9bca404de39bd9aaf97579238005cd15
Alex Chui <ahhchui@gmail.com>
2019-09-10 16:36:21 -0700

    push heroku again

--- frontend/components/note/notebook_index.jsx
+++ frontend/components/note/notebook_index.jsx
@@ -8,8 +8,29 @@ export default class NotebookIndex extends React.Component {
       <h1 className="notebook-index">
         <div className="notebook-index-head">
           <h2>Notebooks</h2>
+          <input type="text" placeholder="Find Notebooks..." />
         </div>
-        
+        <div className="notebook-index-table-head">
+          <h3>My Notebook List</h3>
+            <div className="notebook-index-table-head-buttons">
+              <div className="new-notebook-button">
+                New Notebook 
+              </div>
+              <div className="sort-button">
+                Sort  
+              </div>
+            </div>
+        </div>
+        <div className="notebook-table">
+          <div className="table-labels">
+            <span>Title</span>
+            <span>Created By</span>
+            <span>Updated</span>
+            <span>Shared With</span>
+            <span>Actions</span>
+          </div>
+
+        </div>
       </h1>
     )
   }

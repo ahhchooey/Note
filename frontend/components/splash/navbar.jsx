a9181bfa6392cc62ce5c6b84d3c8ffd39517c62f
Alex Chui <ahhchui@gmail.com>
2019-10-21 15:41:32 -0700

    links

--- frontend/components/splash/navbar.jsx
+++ frontend/components/splash/navbar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {Link} from "react-router-dom";
 
 import Logo from "./logo.jsx";
 import AuthButtons from "./auth_buttons.jsx";
@@ -18,16 +17,19 @@ export default class Navbar extends React.Component {
       <nav className="navbar">
         <Logo className="complete-logo" />
         <div className="links">
-          <Link className="ln plans" to={"/"}>Plans<font className="arrow">▼</font></Link>
-          <Link className="ln features" to={"/"}>Features<font className="arrow">▼</font></Link>
-          <Link className="ln help" to={"/"}>Help & Learning</Link>
-          <div className="ln about" to={"/"}>About Me<font className="arrow">▼</font>
+          <div className="ln plans" to={"/"}>Plans<font className="arrow"></font></div>
+          <div className="ln features" to={"/"}>Features<font className="arrow"></font></div>
+          <div className="ln help" to={"/"}>Help & Learning</div>
+          <div className="ln about about-ln" to={"/"}>About Me<font className="arrow">▼</font>
             <div className="about-dropdown">
               <a href="https://github.com/ahhchooey/Note">
                 <img className="about-me-img" src="https://image.flaticon.com/icons/png/512/25/25231.png" />
               </a>
               <a href="https://github.com/ahhchooey/Note">
                 <img className="about-me-img" src="https://image.flaticon.com/icons/png/512/34/34227.png" />
+              </a>
+              <a href="https://alexchui.dev">
+                <img className="about-me-img" src="http://www.vectorico.com/download/office/folder-icon.png" />
               </a>
             </div>
           </div>
